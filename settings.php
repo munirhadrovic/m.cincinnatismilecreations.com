ababbfbb6567fe804f56ae4631b0294b707132c8
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-05-04 15:27:56 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,19 @@
+<?php
+	$adresa[0]="8005 Plainfield Rd, Ste 31 Cincinnati,OH 45236"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="PLAINFIELD ROAD OFFICE";
+	$telephone[0]=5137918937;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="8464 Winton Road Cincinnati,OH 45231"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="WINTON ROAD OFFICE";
+	$telephone[1]=5139317542;// Broj koji se poziva na dugme CALL US
+
+	
+
+	$companynumber=2;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
